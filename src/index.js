5824ea3ed4c8aafff7f04d0ae2806ca80fbf1725
serge_avila7 <serge_avila7@yahoo.com>
2021-06-18 12:37:00 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const app = express();
 const morgan = require('morgan');
 const cors = require('cors');
-const app = express();
 const bodyparser = require('body-parser');
 require('./database');
 app.set('port', process.env.PORT || 4000);
@@ -12,7 +12,7 @@ app.use(bodyparser.urlencoded({ extended: true }));
 app.use(bodyparser.json());
 app.use(express.json());
 
-app.use(cors({ origin: '*' }));
+app.use(cors({ origen: '*' }));
 app.use('/user', require('./routes/userRoute'));
 app.use('/employee', require('./routes/employeeRoute'));
 

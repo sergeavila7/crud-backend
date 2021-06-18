f19e4fb7dcc10d5fc1614bdb0d821ebc14d0831f
serge_avila7 <serge_avila7@yahoo.com>
2021-06-18 12:55:09 -0500

    make it better

--- src/index.js
+++ src/index.js
@@ -13,6 +13,10 @@ app.use(bodyparser.json());
 app.use(express.json());
 
 app.use(cors({ origin: '*' }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 app.use('/user', require('./routes/userRoute'));
 app.use('/employee', require('./routes/employeeRoute'));
 
